0504a217ce73bdb961b8caa471bca5d2a38d84a0
Roshni batra <roshnib.48@gmail.com>
2020-12-17 18:48:46 +0530

    working code on camera

--- app/src/main/java/com/example/android/customcamera/MainActivity.java
+++ app/src/main/java/com/example/android/customcamera/MainActivity.java
@@ -1,14 +1,204 @@
 package com.example.android.customcamera;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.extensions.BokehImageCaptureExtender;
+import androidx.camera.extensions.HdrImageCaptureExtender;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+//import androidx.camera.extensions.BokehExtender;
+
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private Executor executor = Executors.newSingleThreadExecutor();
+    PreviewView mPreviewView;
+    ImageCapture imageCapture;
+    File outputDirectory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPreviewView = findViewById(R.id.preview_camera);
+
+        if (allPermissionsGranted()) {
+            startCamera(); //start camera if permission has been granted by user
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
+
+    }
+
+
+    public String getBatchDirectoryName() {
+
+        String app_folder_path = "";
+        app_folder_path = Arrays.toString(getExternalMediaDirs());
+        File dir = new File(app_folder_path);
+        if (!dir.exists() && !dir.mkdirs()) {
+            File imageFolder = new File(app_folder_path);
+            imageFolder.mkdirs();
+        }
+
+        return app_folder_path;
+    }
+
+
+    private boolean allPermissionsGranted() {
+
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Please provide permission to access camera", Toast
+                        .LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+
+    private void startCamera() {
+
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                    cameraProvider.unbindAll();
+
+                    bindPreview(cameraProvider);
+
+                } catch (ExecutionException | InterruptedException e) {
+
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .build();
+
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+
+        //Vendor-Extensions (The CameraX extensions dependency in build.gradle)
+        HdrImageCaptureExtender hdrImageCaptureExtender = HdrImageCaptureExtender.create(builder);
+
+        // Query if extension is available (optional).
+        if (hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)) {
+            // Enable the extension if available.
+            hdrImageCaptureExtender.enableExtension(cameraSelector);
+        }
+
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageAnalysis, imageCapture);
+
+        ImageButton capture_photo = findViewById(R.id.capture_picture);
+        capture_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+
+            }
+
+            private void takePhoto() {
+                SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
+
+                File filepath = getExternalCacheDir();
+                assert filepath != null;
+                File dir =new File(filepath.getAbsolutePath());
+                dir.mkdir();
+                File file = new File(dir,mDateFormat.format(new Date())+".jpg");
+
+                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions
+                        .Builder(file).build();
+                imageCapture.takePicture(outputFileOptions, executor, new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Photo saved successfully", Toast.LENGTH_SHORT).show();
+                                String mesg = "Photo capture successful"+ Uri.fromFile(file);
+                                Log.d("MainActivity.this",mesg);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        exception.printStackTrace();
+//                        Toast.makeText(MainActivity.this, "Sorry there was trouble in saving " +
+//                                "your file", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
