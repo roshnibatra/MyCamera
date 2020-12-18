1f9de4b775fe3be152cb01805e412c4c37975212
Roshni batra <roshnib.48@gmail.com>
2020-12-18 08:07:59 +0530

    added google drive upload

---
+++ app/src/main/java/com/example/android/customcamera/DriveServiceHelper.java
@@ -0,0 +1,54 @@
+package com.example.android.customcamera;
+
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class DriveServiceHelper {
+
+    private static final String TAG = "DriveServiceHelper";
+    private final Executor mExecutor = Executors.newSingleThreadExecutor();
+    private Drive mDriveService;
+    Date date = new Date();
+
+
+    public DriveServiceHelper(Drive mDriveService) {
+        this.mDriveService = mDriveService;
+    }
+
+    public Task<String> uploadYourImage(String filepath) {
+        Log.i(TAG, "uploadYourImage: " + filepath);
+        if (filepath.isEmpty()) {
+            Log.i(TAG, "uploadYourImage: got empty file" );
+        }
+        return Tasks.call(mExecutor,() -> {
+
+            File filemetaData = new File();
+            filemetaData.setName(String.valueOf(date.getTime()));
+            java.io.File file = new java.io.File(filepath);
+            FileContent mediaContent = new FileContent("image/jpeg",file);
+            File myFile = null;
+            try {
+                myFile = mDriveService.files().create(filemetaData, mediaContent)
+                        .setFields("id")
+                        .execute();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            if(myFile == null) {
+                throw  new IOException("Nothing in file ");
+            }
+            return myFile.getId();
+        });
+    }
+}
--- app/src/main/java/com/example/android/customcamera/MainActivity.java
+++ app/src/main/java/com/example/android/customcamera/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.android.customcamera;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.FileUtils;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
-import androidx.camera.extensions.BokehImageCaptureExtender;
 import androidx.camera.extensions.HdrImageCaptureExtender;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -17,26 +28,24 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.provider.ContactsContract;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
-//import androidx.camera.extensions.BokehExtender;
-
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
@@ -45,13 +54,18 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private int REQUEST_CODE_PERMISSIONS = 1001;
     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA",
             "android.permission.WRITE_EXTERNAL_STORAGE"};
     private Executor executor = Executors.newSingleThreadExecutor();
     PreviewView mPreviewView;
-    ImageCapture imageCapture;
-    File outputDirectory;
+    DriveServiceHelper driveServiceHelper;
+    File file;
+    Uri uri;
+    File newFile;
+    private final int WAIT_TIME = 1500;
+    private Handler uiHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,30 +73,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPreviewView = findViewById(R.id.preview_camera);
-
+        uiHandler = new Handler();
         if (allPermissionsGranted()) {
             startCamera(); //start camera if permission has been granted by user
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
+        
+        requestSignIn();
 
 
     }
 
+    private void requestSignIn() {
 
-    public String getBatchDirectoryName() {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                .build();
 
-        String app_folder_path = "";
-        app_folder_path = Arrays.toString(getExternalMediaDirs());
-        File dir = new File(app_folder_path);
-        if (!dir.exists() && !dir.mkdirs()) {
-            File imageFolder = new File(app_folder_path);
-            imageFolder.mkdirs();
+        GoogleSignInClient client = GoogleSignIn.getClient(this,signInOptions);
+        startActivityForResult(client.getSignInIntent(),400);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case 400:
+                if(resultCode == RESULT_OK)
+                {
+                    handleSignInIntent(data);
+                }
+                    break;
         }
-
-        return app_folder_path;
     }
 
+    private void handleSignInIntent(Intent data) {
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+                .addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                        GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                                MainActivity.this, Collections.singleton(DriveScopes.DRIVE_FILE));
+
+                        credential.setSelectedAccount(googleSignInAccount.getAccount());
+
+                        Drive googleDriveService = new Drive.Builder(
+                                AndroidHttp.newCompatibleTransport(),
+                                new GsonFactory(),
+                                credential)
+                                .setApplicationName("My Camera App")
+                                .build();
+
+                        driveServiceHelper = new DriveServiceHelper(googleDriveService);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
 
     private boolean allPermissionsGranted() {
 
@@ -122,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
                     bindPreview(cameraProvider);
 
                 } catch (ExecutionException | InterruptedException e) {
-
+                    //wont be necessary
                 }
             }
         }, ContextCompat.getMainExecutor(this));
@@ -142,6 +197,14 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         ImageCapture.Builder builder = new ImageCapture.Builder();
+
+//         BokehImageCaptureExtender bokehImageCapture = new
+//                BokehImageCaptureExtender(builder);
+//
+//        if (bokehImageCapture.isExtensionAvailable(cameraSelector)) {
+//            // Enable the extension if available.
+//            bokehImageCapture.enableExtension(cameraSelector);
+//        }
 
 
         //Vendor-Extensions (The CameraX extensions dependency in build.gradle)
@@ -164,6 +227,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 takePhoto();
+//                Runnable background = new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // This is the delay
+//                        try {
+//                            Thread.sleep(1000);
+//                        }
+//                        catch(InterruptedException ex) {
+//                            Thread.currentThread().interrupt();
+//                        }
+//                        System.out.println("Going to Profile Data");
+//                        uiHandler.post( onUi );
+//                    }
+//                };
+//
+//                new Thread( background ).start();
 
             }
 
@@ -172,9 +251,9 @@ public class MainActivity extends AppCompatActivity {
 
                 File filepath = getExternalCacheDir();
                 assert filepath != null;
-                File dir =new File(filepath.getAbsolutePath());
+                File dir = new File(filepath.getAbsolutePath());
                 dir.mkdir();
-                File file = new File(dir,mDateFormat.format(new Date())+".jpg");
+                file = new File(dir, mDateFormat.format(new Date()) + ".jpg");
 
                 ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions
                         .Builder(file).build();
@@ -185,8 +264,10 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
                                 Toast.makeText(MainActivity.this, "Photo saved successfully", Toast.LENGTH_SHORT).show();
-                                String mesg = "Photo capture successful"+ Uri.fromFile(file);
-                                Log.d("MainActivity.this",mesg);
+                                String mesg = "Photo capture successful: " + Uri.fromFile(file);
+                                uri = Uri.fromFile(file);
+                                Log.d(TAG, "Calling upLoadImage......");
+                                upLoadImage();
                             }
                         });
                     }
@@ -194,11 +275,51 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onError(@NonNull ImageCaptureException exception) {
                         exception.printStackTrace();
-//                        Toast.makeText(MainActivity.this, "Sorry there was trouble in saving " +
-//                                "your file", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Sorry there was trouble in saving " +
+                                "your file", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         });
+    }
+
+    public void upLoadImage() {
+        ProgressDialog progressBar =  new ProgressDialog(MainActivity.this);
+        progressBar.setTitle("Uploading Image to GoogleDrive");
+        progressBar.setMessage("Please Wait ...");
+        progressBar.show();
+        String addressOfFile = "/storage/emulated/0/Android/data/com.example.android.customcamera/cache/20201217233627.jpg";
+        driveServiceHelper.uploadYourImage(file.getPath())
+                .addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        progressBar.dismiss();
+                        // Delete photo from local storage to keep minimum storage usage
+                        newFile = new File(uri.getPath());
+                        newFile.delete();
+                        Log.i(TAG, "onSuccess: delete1: " + newFile.exists());
+                        if (newFile.exists()) {
+                            try {
+                                newFile.getCanonicalFile().delete();
+                                Log.i(TAG, "onSuccess: delete2: " + newFile.exists());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                if (newFile.exists()) {
+                                    getApplicationContext().deleteFile(newFile.getName());
+                                    Log.i(TAG, "onSuccess: delete3: " + newFile.exists());
+                                }
+                            }
+                        }
+                        Toast.makeText(getApplicationContext(),"Uploaded Successfully",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressBar.dismiss();
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),"Check your google api key",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
